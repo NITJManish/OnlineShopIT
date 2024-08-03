e3991c68a1afb44cba9e720ce9972e4ac85c17f8
neera <kumarmanish150799@gmail.com>
2024-08-03 10:49:10 +0530

    manage state using redux, fetching api from backend

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,12 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { productsApi } from './Api/productApi';
+import { configureStore } from "@reduxjs/toolkit";
 
+import userReducer from "./features/userSlice";
+import cartReducer from "./features/cartSlice";
 
-export const store= configureStore({
-    reducer:{
-        [productsApi.reducerPath]:productsApi.reducer,
-    },
-    middleware: (getDefaultMiddleware)=>
-        getDefaultMiddleware().concat([productsApi.middleware]),
-    
+import { productApi } from "./api/productsApi";
+import { authApi } from "./api/authApi";
+import { userApi } from "./api/userApi";
+import { orderApi } from "./api/orderApi";
+
+export const store = configureStore({
+  reducer: {
+    auth: userReducer,
+    cart: cartReducer,
+    [productApi.reducerPath]: productApi.reducer,
+    [authApi.reducerPath]: authApi.reducer,
+    [userApi.reducerPath]: userApi.reducer,
+    [orderApi.reducerPath]: orderApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat([
+      productApi.middleware,
+      authApi.middleware,
+      userApi.middleware,
+      orderApi.middleware,
+    ]),
 });
